49b018531cce5f71f63caacfab8dbe0228b0f517
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2024-08-11 15:52:33 -0700

    Add files via upload

--- iface.c
+++ iface.c
@@ -6,51 +6,61 @@
 
 int main(int argn, char* argv[]){
   if(argn < 5){
-    printf("%s <input device> <output device> <frequency_khz> <mode r/t> [gain] [sample rate]\n", argv[0]);
+    printf("%s <input device> <output device> <frequency_khz> <mode r/t> [buffer size] [gain] [sample rate] [buffer size]\n", argv[0]);
     printf("mode r - reception | mode t - transmit | mode d - DSB tx | good gain values are like 400\n");
     return 0;
   }
+  int bsize = 1500;
 
   int samplerate = 48000;
 
-  if(argv[6] != NULL){
-    samplerate = atoi(argv[6]);
+  if(argv[7] != NULL){
+    samplerate = atoi(argv[7]);
   }
+
+  if(argv[5] != NULL)
+    bsize = atoi(argv[5]);
+
+  if(bsize < 1500)
+    bsize = 1500;
+
+
   if(samplerate < 48000)
     samplerate = 48000;
 
-  init_f_manager(1500, samplerate);
+  int record_sleep = bsize*10;
+  init_f_manager(bsize, samplerate);
 
   double gain = 1;
 
 
-  if(argv[5] != NULL)
-    gain = atof(argv[5]);
+  if(argv[6] != NULL)
+    gain = atof(argv[6]);
   if(gain <= 0)
     gain = 1;
 
 
   double frequency = atof(argv[3]);
-  printf("gain: %f, rate: %d, frequency: %fkhz\n",gain,samplerate,frequency);
+  printf("gain: %f, buffer size: %d, rate: %d, frequency: %fkhz\n",gain, bsize,samplerate,frequency);
 
-  int status = setup_alsa(argv[1], argv[2], 1500, samplerate);
+  int status = setup_alsa(argv[1], argv[2], bsize, samplerate);
 
   if( status < 0)
     return 0;
 
-  short* input = malloc(sizeof(short) * (1500));
-  short* output = malloc(sizeof(short) * (1500));
+  short* input = malloc(sizeof(short) * (bsize));
+  short* output = malloc(sizeof(short) * (bsize));
   int error = 0;
   while( error != -1){
     error = aread(input);
 
     if(argv[4][0] == 'r')
-      amplitude_demodulate(input,output,1500,frequency, gain);
+      amplitude_demodulate(input,output,bsize,frequency, gain);
     else if (argv[4][0] == 'd')
-      DSB_modulate(input,output,1500,frequency,gain);
+      DSB_modulate(input,output,bsize,frequency,gain);
     else
-      amplitude_modulate(input, output,1500,frequency,gain);
-    sync_record(10000);
+      amplitude_modulate(input, output,bsize,frequency,gain);
+    sync_record(record_sleep);
 
     awrite(output);
 
